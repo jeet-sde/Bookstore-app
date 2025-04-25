ebe6d73ee11b225cc3ece6a852d8161b6dae3b70
jeet-sde <sde.jeetjadav@gmail.com>
2025-04-25 15:06:30 +0530

    initial commit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,55 @@ namespace BulkyWeb.Controllers
            List<Category> objCategoryList = _db.categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name.ToLower() == obj.DisplayOrder.ToString()){
+                ModelState.AddModelError("name", "The Display order cannot match the Name.");
+            }
+           
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        public IActionResult Edit(int? id)
+        {
+            if ((id==null || id== 0))
+            {
+                return NotFound();
+            }
+            Category categoryfromDb = _db.categories.Find(id);
+            
+            if (categoryfromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+        
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        }
     }
 }
